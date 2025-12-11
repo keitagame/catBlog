23ea25593b81400ce7a96c1c2bb891e20fbb4495
keita <keitagame0319@icloud.com>
2025-12-11 22:10:06 +0900

    Update app.py

--- app.py
+++ app.py
@@ -37,4 +37,5 @@ def post(slug):
     html = parser.parse()
     return render_template("post.html", content=html)
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
